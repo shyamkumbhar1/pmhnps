6fb26097e3bb257b4c3d33bd1def079a6f5409e0
Shyamkumbhar57 <134919539+Shyamkumbhar57@users.noreply.github.com>
2023-06-19 10:42:12 +0530

    Update

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,16 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 use App\Http\Controllers\SubcriptionController;
 use App\Http\Controllers\RemainingDetailsController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('migration', function() {
+       Artisan::call('migrate:fresh');
+
+       return "Migration Run Successfully";
+    });
 
 Route::get('/', function () {
     return view('welcome');
