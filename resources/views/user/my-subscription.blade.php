a0035722c1899df37df6a6ff8a89bc804bfb6e49
Shyamkumbhar57 <134919539+Shyamkumbhar57@users.noreply.github.com>
2023-06-23 00:10:40 +0530

    Change style of My Subscription Page

--- resources/views/user/my-subscription.blade.php
+++ resources/views/user/my-subscription.blade.php
@@ -1,105 +1,46 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-    <title>Subcription</title>
-    <style>
-        /* The switch - the box around the slider */
-.switch {
-  position: relative;
-  display: inline-block;
-  width: 60px;
-  height: 34px;
-}
-
-/* Hide default HTML checkbox */
-.switch input {
-  opacity: 0;
-  width: 0;
-  height: 0;
-}
-
-/* The slider */
-.slider {
-  position: absolute;
-  cursor: pointer;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background-color: #ccc;
-  -webkit-transition: .4s;
-  transition: .4s;
-}
-
-.slider:before {
-  position: absolute;
-  content: "";
-  height: 26px;
-  width: 26px;
-  left: 4px;
-  bottom: 4px;
-  background-color: white;
-  -webkit-transition: .4s;
-  transition: .4s;
-}
-
-input:checked + .slider {
-  background-color: #2196F3;
-}
-
-input:focus + .slider {
-  box-shadow: 0 0 1px #2196F3;
-}
-
-input:checked + .slider:before {
-  -webkit-transform: translateX(26px);
-  -ms-transform: translateX(26px);
-  transform: translateX(26px);
-}
-
-/* Rounded sliders */
-.slider.round {
-  border-radius: 34px;
-}
-
-.slider.round:before {
-  border-radius: 50%;
-}
-    </style>
-  </head>
-  <body>
-    <h1>My Subcriptions</h1>
-    {{-- {{ $subscriptions }} --}}
-
+@section('content')
     <div class="container">
-     @if (count($subscriptions)>0)
-     <table class="table">
-        <thead>
-          <tr>
-            <th scope="col">Plan Name</th>
-            <th scope="col">Subscription Name</th>
-            <th scope="col">Amount </th>
-                       <th scope="col">Trial Start At</th>
-               {{-- <th scope="col">Auto Renew</th> --}}
-          </tr>
-        </thead>
-        <tbody>
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Dashboard') }}</div>
 
-            @foreach ( $subscriptions as $subscription )
-            <tr>
-                <th scope="row">{{ $subscription->plan->name }}</th>
-                <th scope="row">{{ $subscription->name }}</th>
-                <th scope="row">{{ $subscription->plan->amount }}</th>
-                <th scope="row">{{ $subscription->created_at }}</th>
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+                        <h1><strong>My Subscription :</strong> </h1>
 
-               {{-- <th>
+                        {{-- {{ $subscriptions }} --}}
+
+                        <div class="container">
+                            @if (count($subscriptions) > 0)
+                                <table class="table">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">Plan Name</th>
+                                            <th scope="col">Subscription Name</th>
+                                            <th scope="col">Amount </th>
+                                            <th scope="col">Trial Start At</th>
+                                            <th scope="col">Status</th>
+                                            {{-- <th scope="col">Auto Renew</th> --}}
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+
+                                        @foreach ($subscriptions as $subscription)
+                                            <tr>
+                                                <th scope="row">{{ $subscription->plan->name }}</th>
+                                                <th scope="row">{{ $subscription->name }}</th>
+                                                <th scope="row">{{ $subscription->plan->amount }}</th>
+                                                <th scope="row">{{ $subscription->created_at }}</th>
+                                                <th scope="row">Active</th>
+
+                                                {{-- <th>
                 <label class="switch">
                     @if ($subscription->ends_at == null)
                     <input type="checkbox" id="switcher" checked value="{{ $subscription->name }}">
@@ -112,59 +53,86 @@ input:checked + .slider:before {
                   </label>
                </th> --}}
 
-              </tr>
-            @endforeach
+                                            </tr>
+                                        @endforeach
 
 
-        </tbody>
-      </table>
-@else
-<h4> You are not Subscribe Any Plan</h4>
-     @endif
+                                    </tbody>
+                                </table>
+                            @else
+                                <h4> You are not Subscribe Any Plan</h4>
+                            @endif
 
+                        </div>
+
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
+@endsection
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-
+@section('scripts')
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script>
-     $(document).ready(function () {
-        $('#switcher').click(function (e) {
-            var subscriptionName = $('#switcher').val();
-            // e.preventDefault();
-            if ($(this).is(':checked')) {
-                console.log('checked');
+        $(document).ready(function() {
+
+            /*------------------------------------------
+            --------------------------------------------
+            Country Dropdown Change Event
+            --------------------------------------------
+            --------------------------------------------*/
+            $('#country-dropdown').on('change', function() {
+                var idCountry = this.value;
+                $("#state-dropdown").html('');
                 $.ajax({
-                type: "get",
-                url: "{{ route('subscription.resume') }}",
-                data:  {subscriptionName},
+                    url: "{{ url('api/fetch-states') }}",
+                    type: "POST",
+                    data: {
+                        country_id: idCountry,
+                        _token: '{{ csrf_token() }}'
+                    },
+                    dataType: 'json',
+                    success: function(result) {
+                        $('#state-dropdown').html(
+                            '<option value="">-- Select State --</option>');
+                        $.each(result.states, function(key, value) {
+                            $("#state-dropdown").append('<option value="' + value
+                                .id + '">' + value.name + '</option>');
+                        });
+                        $('#city-dropdown').html('<option value="">-- Select City --</option>');
+                    }
+                });
+            });
 
-                success: function (response) {
-                    console.log(response);
-
-                },
-                 error: function (response) {
-
-                },
-              });
-            } else {
-              $.ajax({
-                type: "get",
-                url: "{{ route('subscription.cancel') }}",
-                data: {subscriptionName},
-                success: function (response) {
-                    console.log(response);
-                },
-                 error: function (response) {
-
-                },
-              });
-
-            }
+            /*------------------------------------------
+            --------------------------------------------
+            State Dropdown Change Event
+            --------------------------------------------
+            --------------------------------------------*/
+            $('#state-dropdown').on('change', function() {
+                var idState = this.value;
+                $("#city-dropdown").html('');
+                $.ajax({
+                    url: "{{ url('api/fetch-cities') }}",
+                    type: "POST",
+                    data: {
+                        state_id: idState,
+                        _token: '{{ csrf_token() }}'
+                    },
+                    dataType: 'json',
+                    success: function(res) {
+                        $('#city-dropdown').html('<option value="">-- Select City --</option>');
+                        $.each(res.cities, function(key, value) {
+                            $("#city-dropdown").append('<option value="' + value
+                                .id + '">' + value.name + '</option>');
+                        });
+                    }
+                });
+            });
 
         });
-     });
     </script>
-  </body>
-</html>
+@endsection
