8f439c05f6f1ae4ebdd4a3815a4192d858f8b02e
Shyamkumbhar57 <134919539+Shyamkumbhar57@users.noreply.github.com>
2023-06-29 23:21:06 +0530

    Admin Panel Setup

--- resources/views/Admin/adminHome.blade.php
+++ resources/views/Admin/adminHome.blade.php
@@ -1,1 +1,26 @@
-Your Are Admin
+@extends('Admin.layouts.app')
+
+@section('content')
+<!-- Content Wrapper. Contains page content -->
+ <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="mb-2 row">
+          <div class="col-sm-6">
+            <h1 class="m-0"></h1>
+          </div><!-- /.col -->
+
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+
+  </div>
+  <!-- /.content-wrapper -->
+
+  @endsection
+
+  @section('scripts')
+
