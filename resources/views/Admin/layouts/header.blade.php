62e31c9b6ca9e68150573d7f4e975e79cf3f5943
Shyamkumbhar57 <134919539+Shyamkumbhar57@users.noreply.github.com>
2023-07-03 11:51:58 +0530

    Add Admin Logout Functionality

--- resources/views/Admin/layouts/header.blade.php
+++ resources/views/Admin/layouts/header.blade.php
@@ -39,10 +39,19 @@
 
       <!-- Messages Dropdown Menu -->
       <li class="nav-item dropdown">
-        <a class="nav-link" data-toggle="dropdown" href="#">
-          <i class="far fa-comments"></i>
-          <span class="badge badge-danger navbar-badge">3</span>
+        <a class="nav-link"  href="#">
+          <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
         </a>
+        
         <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
           <a href="#" class="dropdown-item">
             <!-- Message Start -->
