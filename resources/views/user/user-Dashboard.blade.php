4ce7d2664006376b91b2664bd86b4483f25a9f8d
Shyamkumbhar57 <134919539+Shyamkumbhar57@users.noreply.github.com>
2023-07-13 14:17:46 +0530

    User Dashboard Update

--- resources/views/user/user-Dashboard.blade.php
+++ resources/views/user/user-Dashboard.blade.php
@@ -1,34 +1,113 @@
-@extends('layouts.app')
+@extends('layouts2.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="page-height">
+        <div class="container">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                    @if (session('success'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('success') }}
-                        </div>
-                    @endif
 
-                    {{ __(' Main Dashboard') }}
-                    <div>
-                        <a href="{{route('user.Dashboard.edit')}}"><button>Edit Profile</button></a>
-                    <a href="{{route('user.my.subscription')}}"><button>My Subscription</button></a>
-                    <a href="##"><button>My Reviews </button></a>
-                    </div>
-
+            <div class="mt-5 mb-4 row">
+                <div class="col-md-12">
+                    <h2 class="mb-4 text-center">PHMNPS Dashboard</h2>
+                    
+                   
                 </div>
+
             </div>
         </div>
-    </div>
-</div>
-@endsection
+
+                                 
+
+
+        <!-- MDB -->
+        <!-- <script type="text/javascript" src="{{ asset('src/js/mdb.min.js') }}"></script> -->
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <script>
+            $(document).ready(function() {
+                $('#area_of_expertise_other').change(function() {
+                    var isChecked = $(this).prop('checked');
+                    var otherInputContainer = $('#otherInputContainer');
+                    var otherInput = $('#otherInput');
+
+                    if (isChecked) {
+                        otherInputContainer.show();
+                        otherInput.prop('required', true);
+                    } else {
+                        otherInputContainer.hide();
+                        otherInput.prop('required', false);
+                    }
+                });
+
+
+            });
+        </script>
+        <script>
+            $(document).ready(function() {
+
+                /*------------------------------------------
+                --------------------------------------------
+                Country Dropdown Change Event
+                --------------------------------------------
+                --------------------------------------------*/
+                $('#country-dropdown').on('change', function() {
+                    var idCountry = this.value;
+                    $("#state-dropdown").html('');
+                    $.ajax({
+                        url: "{{ url('api/fetch-states') }}",
+                        type: "POST",
+                        data: {
+                            country_id: idCountry,
+                            _token: '{{ csrf_token() }}'
+                        },
+                        dataType: 'json',
+                        success: function(result) {
+                            $('#state-dropdown').html(
+                                '<option value="">-- Select State --</option>');
+                            $.each(result.states, function(key, value) {
+                                $("#state-dropdown").append('<option value="' + value
+                                    .id + '">' + value.name + '</option>');
+                            });
+                            $('#city-dropdown').html('<option value="">-- Select City --</option>');
+                        }
+                    });
+                });
+
+                /*------------------------------------------
+                --------------------------------------------
+                State Dropdown Change Event
+                --------------------------------------------
+                --------------------------------------------*/
+                $('#state-dropdown').on('change', function() {
+                    var idState = this.value;
+                    $("#city-dropdown").html('');
+                    $.ajax({
+                        url: "{{ url('api/fetch-cities') }}",
+                        type: "POST",
+                        data: {
+                            state_id: idState,
+                            _token: '{{ csrf_token() }}'
+                        },
+                        dataType: 'json',
+                        success: function(res) {
+                            $('#city-dropdown').html('<option value="">-- Select City --</option>');
+                            $.each(res.cities, function(key, value) {
+                                $("#city-dropdown").append('<option value="' + value
+                                    .id + '">' + value.name + '</option>');
+                            });
+                        }
+                    });
+                });
+
+            });
+        </script>
+        <script>
+            function previewImage(event) {
+                var reader = new FileReader();
+                reader.onload = function() {
+                    var output = document.getElementById('preview');
+                    output.src = reader.result;
+                    output.style.display = 'block';
+                };
+                reader.readAsDataURL(event.target.files[0]);
+            }
+        </script>
+    @endsection
