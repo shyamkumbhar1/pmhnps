d9d323b6c19665a8af62e3e4c3fc2706ccf1abba
shyam Kumbhar <shyamkumbhar509@gmail.com>
2024-02-28 14:46:32 +0530

    Update

--- app/Http/Controllers/PracticeController.php
+++ app/Http/Controllers/PracticeController.php
@@ -5,14 +5,23 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\Cookie;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Crypt;
+
 
 
 class PracticeController extends Controller
 {
     public function practice (){
    
+        $plainPassword  = "Shyam Kumbhar Babu  ";
+        // $hashedPassword = Hash::make($plainPassword);
+        $cryptData = Crypt::encrypt($plainPassword);
 
-      
-            return config('constants.name');
+
+         
+        //  return $password ;
+        dd($cryptData);
+     
     }
 }
